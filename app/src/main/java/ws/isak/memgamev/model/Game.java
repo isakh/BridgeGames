c867c839a5974e59497dd60657b237a718ef16c7
Isak Herman <isak@alumni.stanford.edu>
2017-03-02 15:46:33 +0000

    class Game updated class description

--- app/src/main/java/ws/isak/memgamev/model/Game.java
+++ app/src/main/java/ws/isak/memgamev/model/Game.java
@@ -3,7 +3,12 @@ package ws.isak.memgamev.model;
 import ws.isak.memgamev.themes.Theme;
 
 /**
- * This is the instance of an active playing game
+ * This is the instance of an active playing game.  A given game is comprised of: a board configuration
+ * which is defined by the dimensions of the board based on the difficulty selected; a board
+ * arrangement which covers mapping tile pairs and tile IDs to bitmaps; a theme which defines the
+ * set of images (and whether they are identical pairs or non-identical) and audio corresponding to
+ * the theme; and game state which tracks the current performance in terms of time, stars earned, and
+ * corresponding score.
  *
  * @author isak
  */
